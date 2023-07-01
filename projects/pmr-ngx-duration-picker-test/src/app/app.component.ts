540da282378dc4f44e1175454d3f281762dfc360
Pedro Relvas <pedromiguelrelvas@gmail.com>
2023-07-01 23:08:10 +0100

    PMRDP-5 :: added footer

--- projects/pmr-ngx-duration-picker-test/src/app/app.component.ts
+++ projects/pmr-ngx-duration-picker-test/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   latestVersion = '15.0.3';
+  currentYear = new Date().getFullYear();
 }
